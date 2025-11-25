ad9d9d8dea19b868e142835cda242cc017be61a5
sahildhyani9 <sahil9dhyani@gmail.com>
2025-11-25 12:07:26 +0530

    chnages full

--- contact_process.php
+++ contact_process.php
@@ -1,22 +1,25 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+	$to = "rockybd1995@gmail.com";
+	// Form fields
+	$from_user = isset($_REQUEST['email']) ? $_REQUEST['email'] : '';
+	$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : '';
+	$csubject = isset($_REQUEST['subject']) ? $_REQUEST['subject'] : '';
+	$cmessage = isset($_REQUEST['message']) ? $_REQUEST['message'] : '';
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
+	// Mail headers - use a site email as From for better deliverability
+	$site_from = 'no-reply@tanpurahouse.com';
+	$headers = "From: " . $site_from . "\r\n";
+	if (!empty($from_user)) {
+		$headers .= "Reply-To: " . $from_user . "\r\n";
+	}
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+	$mail_subject = "You have a message from your website";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+	$logo = 'img/logo.png';
+	$link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
@@ -24,7 +27,7 @@
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from_user}</td>";
 	$body .= "</tr>";
 	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
@@ -32,6 +35,15 @@
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
+	$send = mail($to, $mail_subject, $body, $headers);
+
+	if ($send) {
+		// Return a simple response for the AJAX caller
+		echo "OK";
+		http_response_code(200);
+	} else {
+		echo "ERROR";
+		http_response_code(500);
+	}
 
 ?>
